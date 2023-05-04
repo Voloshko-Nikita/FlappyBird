684ddf99835638238692e84b39900fa2b3985232
Voloshko-Nikita <nikita.voloshko.music@gmail.com>
2023-05-04 21:15:52 +0300

    Create PipePair.lua

---
+++ Flappy Bird/PipePair.lua
@@ -0,0 +1,35 @@
+
+PipePair = class{}
+
+gap_height = 90
+
+
+function PipePair:init(y)
+    self.x = VIRTUAL_WIDTH + 32
+    self.y = y
+
+    self.pipes = {
+        ['upper'] = Pipe('top', self.y),
+        ['lower'] = Pipe('bottom', self.y + PIPE_HEIGHT + gap_height)
+    }
+    self.remove = false
+    self.scored = false
+
+end
+
+function PipePair:update(dt)
+    if self.x > -PIPE_WIDTH then
+        self.x = self.x - PIPE_SPEED * dt
+        self.pipes['upper'].x = self.x
+        self.pipes['lower'].x = self.x
+    else
+        self.remove = true 
+    end
+    gap_height = math.random(80,90)
+end
+
+function PipePair:render()
+    for k, pipe in pairs(self.pipes) do
+        pipe:render()
+    end
+end
